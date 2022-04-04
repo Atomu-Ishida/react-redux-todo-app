b34af66b0ae2f76eaa8c314704b3f9523a06f5f7
Atomu-Ishida <atomu.10.18@icloud.com>
2022-04-04 18:01:09 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ReduxでTodoリスト作成</h1>
+    </div>
+  );
+}
+
+export default App;
